3a7dece04ac15ca70257dd9e139ad4340e676658
Jacob Bednarz <jacob.bednarz@gmail.com>
2014-09-15 06:53:34 +1000

    Add in some reusable exception classes

---
+++ lib/acquia/exceptions.rb
@@ -0,0 +1,5 @@
+module Acquia
+  class MissingCloudApiCredentials < StandardError; end
+  class MissingNetrcConfiguration < StandardError; end
+  class InvalidUserCredentials < StandardError; end
+end
