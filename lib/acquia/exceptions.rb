2e34096408405d1991965b4d3761a962102abf72
Jacob Bednarz <jacob.bednarz@gmail.com>
2015-02-16 07:38:41 +1000

    Add in ApiRequestError exception

--- lib/acquia/exceptions.rb
+++ lib/acquia/exceptions.rb
@@ -2,4 +2,5 @@ module Acquia
   class MissingCloudApiCredentials < StandardError; end
   class MissingNetrcConfiguration < StandardError; end
   class InvalidUserCredentials < StandardError; end
+  class ApiRequestError < StandardError; end
 end
