2ac37e79b349aeedc9a2beeca1e5128820a0766a
Jacob Bednarz <jacob.bednarz@gmail.com>
2015-02-20 17:12:27 +1000

    Remove InvalidUserCredentials in favour of Faraday exceptions

--- lib/acquia_sdk_ruby/exceptions.rb
+++ lib/acquia_sdk_ruby/exceptions.rb
@@ -1,6 +1,7 @@
 module Acquia
   class MissingCloudApiCredentials < StandardError; end
   class MissingNetrcConfiguration < StandardError; end
-  class InvalidUserCredentials < StandardError; end
+
+  # Generic response error.
   class ApiRequestError < StandardError; end
 end
